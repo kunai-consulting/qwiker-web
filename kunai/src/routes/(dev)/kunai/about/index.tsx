4b4500061e5f3c4020a5e30495b7800cf1d15ccd
alex <zaynet.ua@gmail.com>
2023-12-19 14:30:15 +0200

    Add text expose

--- kunai/src/routes/(dev)/kunai/about/index.tsx
+++ kunai/src/routes/(dev)/kunai/about/index.tsx
@@ -5,7 +5,11 @@ import Story from "~/routes/(dev)/kunai/about/sections/Story";
 export default component$(() => {
   return (
     <>
-      <About />
+      <About
+        title={'We are Kunai'}
+        subtitle1={'Kunai is a boutique product consultancy that has spent two decades transforming the financial services industry.'}
+        subtitle2={'We conceive, design, develop, and operate products & services that are changing the world’s relationship with money.'}
+      />
       <Story />
     </>
   );
--- kunai/src/routes/(dev)/kunai/about/sections/About.tsx
+++ kunai/src/routes/(dev)/kunai/about/sections/About.tsx
@@ -1,7 +1,13 @@
 import {component$} from "@builder.io/qwik";
 import AboutBanner from '~/routes/(public)/img/about-us/about-banner.jpg?jsx';
 
-export default component$(() => {
+interface AboutProps {
+  title: string;
+  subtitle1: string;
+  subtitle2: string;
+}
+
+export default component$<AboutProps>((props) => {
   return (
     <div class='container flex flex-col md:flex-row max-w-7xl gap-5 md:gap-0
     items-center justify-between
@@ -9,14 +15,12 @@ export default component$(() => {
     >
       <div class='flex flex-col items-start gap-2.5 md:gap-[30px] w-full md:w-[47%]'>
         <div class='text-[28px] md:text-[44.801px]'>
-          We are Kunai
+          {props.title}
         </div>
         <div class='text-sm md:text-xl whitespace-pre-line'>
-          Kunai is a boutique product consultancy that has spent two decades transforming the financial services
-          industry.
+          {props.subtitle1}
           <br/><br/>
-          We conceive, design, develop, and operate products & services that are changing the world’s relationship with
-          money.
+          {props.subtitle2}
         </div>
       </div>
       <div class='relative w-full md:w-1/2 rounded'>
