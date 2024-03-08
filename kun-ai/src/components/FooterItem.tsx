c9fd958713ca7e2df7dff365db2bfeb701a7a9ea
toan-kunaico <toan@kunaico.com>
2024-03-07 17:00:52 -0800

    Adjust kun.ai layout and copy

--- kun-ai/src/components/FooterItem.tsx
+++ kun-ai/src/components/FooterItem.tsx
@@ -4,21 +4,23 @@ import ArrForward from '/public/arrow_forward.svg?jsx';
 export default component$(() => {
   return (
     <div
-      class='-ml-1 mt-0 lg:mt-8 flex flex-col place-content-between gap-8 p-8
-              border rounded-2xl border-[#858891] lg:border-none lg:rounded-none
-              bg-[url("/footer-small.svg")] lg:bg-[url("/footer.svg")] bg-no-repeat'
+      class='-ml-1 mt-0 flex flex-col place-content-between gap-8 rounded-2xl border
+              border-[#858891] bg-[url("/footer.svg")] max-lg:bg-[70%_80%] bg-no-repeat
+              p-8 lg:mt-8'
     >
-      <div class={'pb-10 md:pb-14 text-2xl md:text-3xl md:leading-[50px] font-bold text-[#D5D5D5]'}>
+      <div
+        class={
+          'pb-10 text-2xl font-bold text-[#D5D5D5] md:pb-14 md:text-3xl md:leading-[50px]'
+        }
+      >
         <h2>
-          Step into the future with{' '}
-          <span class={'text-[#8F66FF]'}>Kun.AI</span>:
+          Start your journey with <span class={'text-[#8F66FF]'}>Kun.AI</span>:
         </h2>
-        <h2>Streamlined workflows, boosted</h2>
-        <h2>efficiency, and cost savings</h2>
+        <h2>Automate Your Business</h2>
       </div>
       <button
-        class="mb-2 lg:mb-12 flex h-12 w-44 flex-row place-content-center items-center rounded border border-[#8F66FF]
-          font-semibold uppercase text-[#D5D5D5] focus:ring-2 focus:ring-blue-800"
+        class="mb-2 flex h-12 w-44 flex-row place-content-center items-center rounded border border-[#8F66FF] font-semibold
+          uppercase text-[#D5D5D5] focus:ring-2 focus:ring-blue-800 lg:mb-12"
       >
         Let's try it
         <ArrForward class={'ml-3.5'} width={16} height={16} />
--- kun-ai/src/components/HeaderItem.tsx
+++ kun-ai/src/components/HeaderItem.tsx
@@ -3,8 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <div class={'w-full text-3xl sm:text-5xl font-bold leading-[47px] md:leading-[67px] text-[#D5D5D5]'}>
-      <h1>Elevate Your Work to</h1>
-      <h1>Unprecedented Heights </h1>
+      <h1>Automate Real Work</h1>
       <h1>
         with <span class='text-[#8F66FF]'>Kun.AI</span>
       </h1>
