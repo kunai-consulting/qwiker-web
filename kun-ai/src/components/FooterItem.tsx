c910a97a7cb9e50a5448eb41814279fc772e6a84
toan-kunaico <122486240+toan-kunaico@users.noreply.github.com>
2024-03-08 12:32:16 -0800

    layout adjustment (#38)
    * layout adjustment
    
    * fix card gap

--- kun-ai/src/components/FooterItem.tsx
+++ kun-ai/src/components/FooterItem.tsx
@@ -5,8 +5,8 @@ export default component$(() => {
   return (
     <div
       class='-ml-1 mt-0 flex flex-col place-content-between gap-8 rounded-2xl border
-              border-[#858891] bg-[url("/footer.svg")] max-lg:bg-[70%_80%] bg-no-repeat
-              p-8 lg:mt-8'
+              border-[#858891] bg-[url("/footer.svg")] bg-no-repeat p-8
+              max-lg:bg-[70%_80%] lg:mt-8'
     >
       <div
         class={
@@ -18,13 +18,14 @@ export default component$(() => {
         </h2>
         <h2>Automate Your Business</h2>
       </div>
-      <button
+      <a
+        href="mailto:ss@kun.ai"
         class="mb-2 flex h-12 w-44 flex-row place-content-center items-center rounded border border-[#8F66FF] font-semibold
           uppercase text-[#D5D5D5] focus:ring-2 focus:ring-blue-800 lg:mb-12"
       >
         Let's try it
         <ArrForward class={'ml-3.5'} width={16} height={16} />
-      </button>
+      </a>
     </div>
   );
 });
